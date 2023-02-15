c5492a5ad69224c57dcd83cc0289b192d53f76e9
hippienft <107660929+hippienft@users.noreply.github.com>
2023-02-15 10:15:03 -0800

    docs: Addition of card images

--- lib/screens/black_jack_screen.dart
+++ lib/screens/black_jack_screen.dart
@@ -10,6 +10,93 @@ class BlackJackScreen extends StatefulWidget {
 class _BlackJackScreenState extends State<BlackJackScreen> {
   bool _isStarted = false;
 
+  // Card Images
+  List<Image> myCards = [];
+
+  List<Image> dealerCards = [];
+
+  // Cards
+  String? dealerFirstCard;
+  String? dealerSecondCard;
+
+  String? playerFirstCard;
+  String? playerSecondCard;
+
+  // Scores
+  int dealerScore = 0;
+  int playerScore = 0;
+
+  // Deck Of Cards
+  final Map<String, int> deckOfCards = {
+    "cards/2.1.png": 2,
+    "cards/2.2.png": 2,
+    "cards/2.3.png": 2,
+    "cards/2.4.png": 2,
+    "cards/3.1.png": 3,
+    "cards/3.2.png": 3,
+    "cards/3.3.png": 3,
+    "cards/3.4.png": 3,
+    "cards/4.1.png": 4,
+    "cards/4.2.png": 4,
+    "cards/4.3.png": 4,
+    "cards/4.4.png": 4,
+    "cards/5.1.png": 5,
+    "cards/5.2.png": 5,
+    "cards/5.3.png": 5,
+    "cards/5.4.png": 5,
+    "cards/6.1.png": 6,
+    "cards/6.2.png": 6,
+    "cards/6.3.png": 6,
+    "cards/6.4.png": 6,
+    "cards/7.1.png": 7,
+    "cards/7.2.png": 7,
+    "cards/7.3.png": 7,
+    "cards/7.4.png": 7,
+    "cards/8.1.png": 8,
+    "cards/8.2.png": 8,
+    "cards/8.3.png": 8,
+    "cards/8.4.png": 8,
+    "cards/9.1.png": 9,
+    "cards/9.2.png": 9,
+    "cards/9.3.png": 9,
+    "cards/9.4.png": 9,
+    "cards/10.1.png": 10,
+    "cards/10.2.png": 10,
+    "cards/10.3.png": 10,
+    "cards/10.4.png": 10,
+    "cards/J1.png": 10,
+    "cards/J2.png": 10,
+    "cards/J3.png": 10,
+    "cards/J4.png": 10,
+    "cards/Q1.png": 10,
+    "cards/Q2.png": 10,
+    "cards/Q3.png": 10,
+    "cards/Q4.png": 10,
+    "cards/K1.png": 10,
+    "cards/K2.png": 10,
+    "cards/K3.png": 10,
+    "cards/K4.png": 10,
+    "cards/A1.png": 11,
+    "cards/A2.png": 11,
+    "cards/A3.png": 11,
+    "cards/A4.png": 11,
+  };
+
+  Map<String, int> playingCards = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    playingCards.addAll(deckOfCards);
+  }
+
+  //Reset the Rounds and Reset Cards
+  void changeCard() {}
+
+  //Add Extra Card to Player
+  void addCard() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +126,7 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
                             gridDelegate:
                                 SliverGridDelegateWithFixedCrossAxisCount(
                                     crossAxisCount: 3),
+                            itemCount: 0,
                             itemBuilder: (context, index) {
                               //Dealer's Cards
                               return Container();
@@ -59,6 +147,7 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
                             gridDelegate:
                                 SliverGridDelegateWithFixedCrossAxisCount(
                                     crossAxisCount: 3),
+                            itemCount: 0,
                             itemBuilder: (context, index) {
                               //Player's Cards
                               return Container();
